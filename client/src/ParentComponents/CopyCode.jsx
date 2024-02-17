8f05bdb095272d25f3df96ab6895ef56f763e6eb
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-02-17 17:42:57 -0600

    working on simple styling of code copy components

---
+++ client/src/ParentComponents/CopyCode.jsx
@@ -0,0 +1,17 @@
+const CopyCode = ({ code }) => {
+    const clipBoard = () => {
+        navigator.clipboard.writeText(code).then(() => {
+            // alert("copied!")
+        }, (err) => {
+            console.error("Could not copy", err)
+        })
+    }
+    return (
+        <div className="copy-container">
+            <pre><code>{code}</code></pre>
+            <button onClick={clipBoard} className="copy-button">Copy</button>
+        </div>
+    )
+}
+
+export default CopyCode;

60582b54aced993c97e3f6f64f5a7b6cd8935c5f
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2024-02-17 00:02:09 -0600

    added links to homebrew site and visual studio site for downloading

---
+++ client/src/ChildrenComponents/EnvironmentEnv.jsx
@@ -0,0 +1,22 @@
+const EnvironmentEnv = () => {
+    return (
+        <div>
+            <br/>
+            <h1>Env Section: I would like to specify this is for MacOS currently. I will update for Windows as well in the future</h1>
+            <br/>
+            <div>
+                <h1>Homebrew</h1>
+                <a href="https://brew.sh/">Homebrew Site</a>
+                <p>process: go to site, run command in terminal. To find the terminal, simply use <b>'Command Spacebar', type in 'Terminal' </b></p>
+            </div>
+            <div>
+                <h1>Visual Studio Code</h1>
+                <a href="https://code.visualstudio.com/">VSCode Site</a>
+                <p>process: click file for your computer model, follow download process</p>
+            </div>
+        </div>
+    )
+}
+
+export default EnvironmentEnv;
+
--- client/src/ParentComponents/App.jsx
+++ client/src/ParentComponents/App.jsx
@@ -6,6 +6,11 @@ import Language from './Main/Language'
 import Framework from './Main/Framework'
 import Database from './Main/Database'
 import Header from './Header'
+import EnvironmentEnv from '../ChildrenComponents/EnvironmentEnv'
+import EnvironmentNode from '../ChildrenComponents/EnvironmentNode'
+import EnvironmentRuby from '../ChildrenComponents/EnvironmentRuby'
+import EnvironmentPython from '../ChildrenComponents/EnvironmentPython'
+import EnvironmentSQL from '../ChildrenComponents/EnvironmentSQL'
 
 function App() {
 
@@ -13,6 +18,11 @@ function App() {
     <div>
       <Header/>
       <Routes>
+        <Route path="/env" element={<EnvironmentEnv />}/>
+        <Route path="/env/node" element={<EnvironmentNode />}/>
+        <Route path="/env/ruby" element={<EnvironmentRuby />}/>
+        <Route path="/env/python" element={<EnvironmentPython />}/>
+        <Route path="/env/sql" element={<EnvironmentSQL />}/>
         <Route path="/databases" element={<Database />}/>
         <Route path="/frameworks" element={<Framework />}/>
         <Route path="/languages" element={<Language />}/>
